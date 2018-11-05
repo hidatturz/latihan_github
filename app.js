1a270e6319d68f4e50ddf39d9f6b1867792bdbb9
lintangwisesa <lintangwisesa@ymail.com>
2018-11-05 10:43:05 +0700

    satu

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// v 1.1 done! launch!
+
+var pangkat = (x, y) => {
+    return Math.pow(x,y)
+}
+
+console.log(pangkat(2,3))
+
+// 1 function recursive pangkat
+// 2 function return x ** y
+// 3 func return Math.pow(x,y)
